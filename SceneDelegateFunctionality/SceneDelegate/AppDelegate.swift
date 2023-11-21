5c26b879864caac0ed7c856d4b67a9d0af74069b
Norbert Grover <norbert298@gmail.com>
2023-11-21 08:51:58 -0800

    Changes for scenedelegate

--- SceneDelegateFunctionality/SceneDelegate/AppDelegate.swift
+++ SceneDelegateFunctionality/SceneDelegate/AppDelegate.swift
@@ -1,8 +1,11 @@
-//
-//  AppDelegate.swift
-//  SceneDelegateFunctionality
-//
-//  Created by Norbert Grover on 11/21/23.
-//
+import UIKit
 
-import Foundation
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+    
+}
+
--- SceneDelegateFunctionality/SceneDelegateFunctionalityApp.swift
+++ SceneDelegateFunctionality/SceneDelegateFunctionalityApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct SceneDelegateFunctionalityApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .background:
+                print("Moved to background")
+            case .inactive:
+                print("I'm innactive")
+            case .active:
+                print("I'm active")
+            default:
+                break
+            }
+        }
     }
+        
 }
